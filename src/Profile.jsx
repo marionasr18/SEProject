a56bb424289e84b1f171161e3836b3808be0403e
mario.nasr <Mario.Nasr@ctserv.net>
2023-03-22 16:43:49 +0200

    ok

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -38,7 +38,7 @@ const drawArticles = useCallback(()=>{
   debugger
   return state.articles.map(e=>{
     return(<>
-      <a href={`#${e.url}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <a href={`#${e.url}`} className="flex col-4 items-center bg-white border border-gray-200 rounded-lg shadow  md:max-w-xl">
           <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={e.urlToImage} alt=""/>
           <div className="flex flex-col justify-between p-4 leading-normal">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{e.content}</h5>
