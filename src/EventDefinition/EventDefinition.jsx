8343a520eae782a87b053aa71789dbe04d40310e
marionasr18 <55279029+marionasr18@users.noreply.github.com>
2023-04-10 22:48:29 +0300

    create game

--- src/EventDefinition/EventDefinition.jsx
+++ src/EventDefinition/EventDefinition.jsx
@@ -5,7 +5,9 @@ import '../App.css'
 import { LoadingContext } from "../LoadingContextWrapper";
 import Footer from "../Footer";
 import Select from 'react-select';
+import DatePicker from "react-datepicker";
 import NavigationBar from "../NavigationBar";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 
@@ -21,6 +23,7 @@ const EventDefinition = () => {
         fieldsDesc: '',
         fieldsLocation: '',
         sports: '',
+        tabid: 1,
         sportsOptions: [{ value: 'bas', label: 'Baskteball' },
         { value: 'voll', label: 'Volley Ball' },
         { value: 'fut', label: 'Futsal' },
@@ -28,6 +31,8 @@ const EventDefinition = () => {
     }
 
     const [state, setState] = useState(STATE)
+    const [dateOfEvent, setDateOfEvent] = useState(new Date());
+
     const FillData = useCallback(async () => {
         let data = await FetchData('DataFiles/PlayersData.json', 'get')
         console.log(data.data)
@@ -37,6 +42,7 @@ const EventDefinition = () => {
     }, [])
     console.log(state)
     const handleChange = useCallback((e) => {
+        debugger
         setState(prv => {
             return {
                 ...prv,
@@ -44,25 +50,73 @@ const EventDefinition = () => {
             }
         })
     }, [])
-    const handleBack = useCallback(() => {
-        nav(-1)
+    const toggleNav = useCallback((id) => {
+        setState((prv) => {
+            return {
+                ...prv,
+                tabid: id
+            }
+        })
     }, [])
+
 
     return (
         <>
             <NavigationBar />
-            <div className="row mt-2 ml-5">
-                <div className="offset-4">
-                <input type="radio" value={state.JoinCreate} id="create"
-              onChange={handleChange} name="gender" />
-            <label for="male">Create</label>
+            <div className="row">
+                <div className="col-6 offset-4">
+                    <nav className="navbar navbar-expand-lg navbar-light">
+                        <ul className="navbar-nav">
+                            <li className={state.tabid === 1 ? "nav-item active text-danger" : "nav-item"}>
+                                <a className="nav-link" onClick={() => toggleNav(1)}>Create New Game <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className={state.tabid === 2 ? "nav-item active text-danger" : "nav-item"}>
+                                <a className="nav-link" onClick={() => toggleNav(2)}>Join Game <span className="sr-only">(current)</span></a>
+                            </li>
 
-            <input type="radio" value={state.JoinCreate} id="join"
-              onChange={handleChange} name="gender"/>
-            <label for="Join">Join</label>
-
+                        </ul>
+                    </nav>
                 </div>
             </div>
+            {
+                state.tabid === 1 &&
+                <>
+                    <div className="row mt-2 ml-2">
+                        <div className="col-2">Select Sport to play</div>
+                        <div className="col-3">
+                            <Select
+                                defaultValue={state.sports}
+                                onChange={handleChange}
+                                options={state.sportsOptions}
+                            />
+                        </div>
+                    </div>
+                    <div className="row mt-2 ml-2">
+                        <div className="col-2">Select Capacity</div>
+                        <div className="col-4">
+                            <input type="text" className="form-control  " value={state.capacity} name="capacity" onChange={handleChange} ></input>
+                        </div>
+                    </div>
+                    <div className="row mt-2 ml-2">
+                        <div className="col-2">Location</div>
+                        <div className="col-4">
+                            <input type="text" className="form-control " value={state.location} name="location" onChange={handleChange} ></input>
+                        </div>
+                    </div>
+                    <div className="row mt-2 ml-2">
+                        <div className="col-2">Date</div>
+                        <div className="col-4">
+                        <DatePicker className="form-control" selected={dateOfEvent} onChange={(date) => setDateOfEvent(date)} />
+                        </div>
+                    </div>
+                    </>
+
+            }
+            {
+                state.tabid === 2 &&
+                <div className="row ml-2 mt-3 title">here you can Join games </div>
+
+            }
         </>
     )
 }
