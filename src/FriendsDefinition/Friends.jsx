8e5460f742b3a75ffd9421d241adeb313aec774f
OmarChaiban <omarchaiban21@gmail.com>
2023-05-05 13:54:15 +0300

    visuals

--- src/FriendsDefinition/Friends.jsx
+++ src/FriendsDefinition/Friends.jsx
@@ -36,7 +36,7 @@ const Friends = () => {
                            
                         </div>
                         <div className="flex flex-col items-center pb-10">
-                            <img className="w-24 h-24  rounded-full shadow-lg" src={e.image} alt="No img" />
+                            <img className="w-24 h-24  rounded-full shadow-lg" src={e.profile_picture} alt="No img" />
                             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{e.username } </h5>
                             <span className="text-sm text-gray-500 dark:text-gray-400">{e.address}</span>
                             <div className="flex mt-4 space-x-3 md:mt-6">
